7f904ae184f5627c8c2752fde37a37f7994991a5
a.shamsi <a.shamsi@tosantechno.com>
2023-10-30 12:38:17 +0330

    change project structure, add marketing part

--- container/src/bootstrap.js
+++
@@ -1,6 +0,0 @@
-import {mount} from 'products/ProductsIndex';
-import {mount as Mount} from 'cart/CartShow';
-
-
-mount(document.querySelector('#my-products'))
-Mount(document.querySelector('#my-cart'))
