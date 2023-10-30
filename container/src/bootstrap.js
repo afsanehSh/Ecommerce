5158e29e2111096c221fefc932f558aeed713a9c
a.shamsi <a.shamsi@tosantechno.com>
2023-10-30 12:03:03 +0330

    seprate different apps in project

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,2 +1,6 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import {mount} from 'products/ProductsIndex';
+import {mount as Mount} from 'cart/CartShow';
+
+
+mount(document.querySelector('#my-products'))
+Mount(document.querySelector('#my-cart'))
